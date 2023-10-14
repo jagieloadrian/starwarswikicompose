9ac65548f38fcf94941fd030e1baebb76f61ed1e
diether18 <jagielo.adrian@gmail.com>
2023-10-14 02:16:25 +0200

    feature 3 add music

--- app/src/main/java/com/anjo/starwarswikicompose/MainActivity.kt
+++ app/src/main/java/com/anjo/starwarswikicompose/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.anjo.starwarswikicompose
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -40,8 +48,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val lifecycleOwner = LocalLifecycleOwner.current
+            val current = LocalContext.current
+            val paused = remember{mutableStateOf(false)}
+            val player1: MediaPlayer = remember {MediaPlayer.create(current, R.raw.cantinaband)}
+
+            BackgroundMusic(lifecycleOwner, player1, paused)
+
             StarWarsWikiComposeTheme {
-                Greeting()
                 navController = rememberNavController()
                 SetupNavGraph(navController = navController,
                         startDestination = if (completed) Screen.Home.route else Screen.Welcome.route,
@@ -55,11 +69,26 @@ class MainActivity : ComponentActivity() {
         }
 
     }
-}
 
-
-@Composable
-fun Greeting(mainViewModel: MainViewModel = hiltViewModel()) {
-    val context = LocalContext.current
-    mainViewModel.playSound(context)
+    @Composable
+    private fun BackgroundMusic(lifecycleOwner: LifecycleOwner, player1: MediaPlayer,
+                          paused: MutableState<Boolean>) {
+        DisposableEffect(key1 = lifecycleOwner) {
+            val observer = LifecycleEventObserver { _, event ->
+                if (event == Lifecycle.Event.ON_RESUME ||
+                        event == Lifecycle.Event.ON_CREATE ||
+                        event == Lifecycle.Event.ON_START) {
+                    player1.start()
+                    paused.value = false
+                } else if (event == Lifecycle.Event.ON_STOP) {
+                    player1.pause()
+                    paused.value = true
+                }
+            }
+            lifecycleOwner.lifecycle.addObserver(observer)
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
+        }
+    }
 }
